ba17ad05ead96f16fab1c845c29d204a6efc5f4a
You Jing Web <105593928+YouJingWeb@users.noreply.github.com>
2022-12-18 21:46:57 +0800

    update all over again

--- tanyu/js/main.js
+++ tanyu/js/main.js
@@ -1,22 +1,22 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]');
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+    // const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
+    // sections.forEach(current =>{
+        // const sectionHeight = current.offsetHeight,
+              // sectionTop = current.offsetTop - 58,
+              // sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__list a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__list a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+        // if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            // document.querySelector('.nav__list a[href*=' + sectionId + ']').classList.add('active-link')
+        // }else{
+            // document.querySelector('.nav__list a[href*=' + sectionId + ']').classList.remove('active-link')
+        // }
+    // })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 /* If you do not require the indicator activated by scrolling sections. 
    Delete the main tag code in HTML and the scroll sections code in 
@@ -26,11 +26,11 @@ window.addEventListener('scroll', scrollActive)
 */
 
 /*=============== ACTIVE LINK ===============*/
-// const navlink = document.querySelectorAll('.nav__link')
+ const navlink = document.querySelectorAll('.nav__link')
 
-// function activeLink(){
-//     navlink.forEach((item) => item.classList.remove('active-link'))
-//     this.classList.add('active-link')
-// }
+ function activeLink(){
+     navlink.forEach((item) => item.classList.remove('active-link'))
+     this.classList.add('active-link')
+ }
 
-// navlink.forEach((item) => item.addEventListener('click', activeLink))
+ navlink.forEach((item) => item.addEventListener('click', activeLink))
